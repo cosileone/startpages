7fcca19c0f10a360989e559b25c45654870572d6
Cosi <Cosi@Cosis-ProBook2.local>
2015-02-01 00:15:24 -0500

    Added youtube functionality to search

--- Gaby/js/search.js
+++ Gaby/js/search.js
@@ -1,8 +1,6 @@
-<script type="text/javascript">
 function dosearch() {
-var sf=document.searchform;
-var submitto = sf.sengines.options[sf.sengines.selectedIndex].value + escape(sf.searchterms.value);
-window.location.href = submitto;
-return false;
+	var sf=document.searchform;
+	var submitto = sf.sengines.options[sf.sengines.selectedIndex].value + escape(sf.searchterms.value);
+	window.location.href = submitto;
+	return false;
 }
-</script>
